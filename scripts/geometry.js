29033a012e29ae0b17ce169966a42a47fab0f5c8
Mohammed Abdullhak <m_abdulhak@outlook.com>
2021-09-27 20:30:21 -0230

    Fix Geometry Polygon Undefined Error

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -321,17 +321,21 @@ function pointIsInsidePolygon(point, polygon) {
   const { x } = point; const { y } = point;
 
   let inside = false;
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-    const xi = polygon[i][0]; const yi = polygon[i][1];
-    const xj = polygon[j][0]; const yj = polygon[j][1];
+  try {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+      const xi = polygon[i][0]; const yi = polygon[i][1];
+      const xj = polygon[j][0]; const yj = polygon[j][1];
 
-    const intersect = ((yi > y) !== (yj > y))
-            && (x < ((xj - xi) * (y - yi)) / (yj - yi) + xi);
-    if (intersect) inside = !inside;
+      const intersect = ((yi > y) !== (yj > y))
+              && (x < ((xj - xi) * (y - yi)) / (yj - yi) + xi);
+      if (intersect) inside = !inside;
+    }
+
+    return inside;
+  } catch (err) {
+    return false;
   }
-
-  return inside;
 }
 
 function polygonArea(polygon) {
