cfcfa84d65f7bc01ba2e369fbfca1668cf0505ee
Mohammed Abdullhak <m_abdulhak@outlook.com>
2023-07-06 23:18:46 -0230

    Fix jsgraphs import

--- src/scenes/Cluster/benchmarking/percentageCompletionTracker.js
+++ src/scenes/Cluster/benchmarking/percentageCompletionTracker.js
@@ -1,7 +1,7 @@
 import Tracker from '@common/benchmarking/performanceTrackers/tracker';
 
 // CAN'T GET jsgraphs to work!
-//import jsgraphs from 'js-graph-algorithms';
+const jsgraphs = require('js-graph-algorithms');
 
 class PercentageCompletionTracker extends Tracker {
   constructor() {
@@ -14,33 +14,32 @@ class PercentageCompletionTracker extends Tracker {
     );
 
     this.getValue = (scene) => {
+      // return 42;
 
-      return 42;
-      /*
       const puckDistanceThreshold = 5 * scene.pucks[0].radius;
 
-      let n = scene.pucks.length;
+      const n = scene.pucks.length;
 
       // Construct the graph of pucks with edges connecting those pucks which
       // lie within 'puckDistanceThreshold' of each other.
-      let graph = new jsgraphs.Graph(n);
+      const graph = new jsgraphs.Graph(n);
       for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
-          let dx = scene.pucks[i].position.x - scene.pucks[j].position.x;
-          let dy = scene.pucks[i].position.y - scene.pucks[j].position.y;
-          let dist = Math.sqrt(dx * dx + dy * dy);
+          const dx = scene.pucks[i].position.x - scene.pucks[j].position.x;
+          const dy = scene.pucks[i].position.y - scene.pucks[j].position.y;
+          const dist = Math.sqrt(dx * dx + dy * dy);
           if (dist < puckDistanceThreshold) {
             graph.addEdge(i, j);
           }
         }
       }
 
-      let cc = new jsgraphs.ConnectedComponents(graph);
-      let nComponents = cc.componentCount();
+      const cc = new jsgraphs.ConnectedComponents(graph);
+      const nComponents = cc.componentCount();
 
       // Figure out how many nodes belong to each component.  First create
       // an array with a count of zero for each component.
-      let counts = []
+      const counts = [];
       for (let i = 0; i < nComponents; i++) {
         counts.push(0);
       }
@@ -59,10 +58,9 @@ class PercentageCompletionTracker extends Tracker {
         }
       }
 
-      //return [nComponents, largestCount];
+      // return [nComponents, largestCount];
 
       return (100 * largestCount) / n;
-      */
     };
   }
 }
