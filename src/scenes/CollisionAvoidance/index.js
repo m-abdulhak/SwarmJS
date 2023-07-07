7ffab0de3b334403a70a6adcf4274ec0560267ab
avardy <av@mun.ca>
2023-07-07 12:03:18 -0230

    Added description

--- src/scenes/CollisionAvoidance/index.js
+++ src/scenes/CollisionAvoidance/index.js
@@ -92,13 +92,13 @@ const benchmarkConfig = {
 };
 
 const description = {
-  html: `<p>Object sorting using Buffered Voronoi Cells (BVC).  Each robot chooses an intermediate goal location within its BVC.  This avoids the possibility of collision or conflict with other robots.  Their goal is to incremental shift the pucks towards their respective goal locations.</p>
+  html: `<p>Distributed collision avoidance algorithm based on the concept of Buffered Voronoi Cells (BVC). This novel algorithm predicts and avoids deadlock configurations.</p>
 
-  <p>Rather than pushing pucks directly towards their goals, the robots make use of a goal map for each type of pucks.  A goal map specifies the direction a puck should be pushed in order to reach the goal, accounting for obstacles that might be in the way.</p>
+  <p>In this experiment, the robots each try to reach the opposite point on the circle from which they begin.</p>
 
   <p>
-  <a href=https://link.springer.com/chapter/10.1007/978-3-031-20176-9_27 target=_blank>
-  Abdullhak, Mohammed, and Andrew Vardy. "Distributed Sorting in Complex Environments." Swarm Intelligence: 13th International Conference, ANTS 2022, Málaga, Spain, November 2–4, 2022, Proceedings. Cham: Springer International Publishing, 2022.
+  <a href=https://ieeexplore.ieee.org/document/9636609 target=_blank>
+  Abdullhak, Mohammed, and Andrew Vardy. "Deadlock prediction and recovery for distributed collision avoidance with buffered voronoi cells." 2021 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS). IEEE, 2021.
   </a>
   </p>
   `
